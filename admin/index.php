ada0f9daaa2f28cf1ad1a4f820f9a76bde782738
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-04-26 13:48:57 +0530

    Sub Category

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,10 @@
     <input type="text" class="form-control" id="exampleInputEmail1" name="categoryName" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
+    <label for="categoryname" class="form-label">Subcategory</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" name="SubCategory" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Category Description</label>
     <textarea class="form-control" id="usertext" rows="4" name="categoryDesc"></textarea>
   </div>
@@ -25,15 +29,16 @@
     if(isset($_POST['addCategory']))
     {
         $catname = $_POST['categoryName'];
+        $subcat = $_POST['SubCategory'];
         $catdesc = $_POST['categoryDesc'];
 
-        $que = "INSERT INTO forum_category(category_name,category_desc) VALUES('$catname','$catdesc')";
+        $que = "INSERT INTO forum_category(category_name,category_sub,category_desc) VALUES('$catname','$subcat','$catdesc')";
         $insque = mysqli_query($con,$que);
 
-        /*if($insque){
-            echo "Success";
-        }else {
-            echo "Fail";
-        }*/
+        if($insque){
+            echo "<script>alert('Category Added Successfully!');</script>";
+          }else {
+          echo "<script>alert('Please Try Again!');</script>";
+        }
     }
 ?>
